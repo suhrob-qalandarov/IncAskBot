3c837f96431fc2f466c543c2ee32fc533f3ac3ed
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-25 09:39:48 +0500

    Remove unused Telegram Bot API integration from IncAskBotApplication.

--- src/main/java/org/exp/incaskbot/IncAskBotApplication.java
+++ src/main/java/org/exp/incaskbot/IncAskBotApplication.java
@@ -1,25 +1,14 @@
 package org.exp.incaskbot;
 
-import com.pengrad.telegrambot.TelegramBot;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
 public class IncAskBotApplication {
 
-    @Value("${telegram.bot.token}")
-    private String token;
-
     public static void main(String[] args) {
         SpringApplication.run(IncAskBotApplication.class, args);
-    }
-
-    @Bean
-    public TelegramBot botConfig() {
-        return new TelegramBot(token);
     }
 }
