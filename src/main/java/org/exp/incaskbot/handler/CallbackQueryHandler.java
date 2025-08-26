1b15b1ff126a4402a58d8d4653d2b65609223320
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-26 18:26:05 +0500

    Enhance `CallbackQueryHandler` to handle "delete_message_wait" action; integrate session and message services for improved state management.

--- src/main/java/org/exp/incaskbot/handler/CallbackQueryHandler.java
+++ src/main/java/org/exp/incaskbot/handler/CallbackQueryHandler.java
@@ -1,8 +1,13 @@
 package org.exp.incaskbot.handler;
 
 import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.request.DeleteMessage;
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
+import org.exp.incaskbot.model.Session;
+import org.exp.incaskbot.model.enums.State;
+import org.exp.incaskbot.service.face.MessageService;
+import org.exp.incaskbot.service.face.SessionService;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
@@ -12,8 +17,23 @@ import java.util.function.Consumer;
 @RequiredArgsConstructor
 public class CallbackQueryHandler implements Consumer<CallbackQuery> {
 
+    private final SessionService sessionService;
+    private final MessageService messageService;
+
     @Override
     public void accept(CallbackQuery callbackQuery) {
+        String data = callbackQuery.data();
+        Session session = sessionService.getOrCreateSession(callbackQuery.from());
 
+        if (data.equals("delete_message_wait") && session.getState().equals(State.MESSAGE)) {
+            messageService.deleteMessage(session.getChatId(), session.getLastMessageId());
+            sessionService.updateSessionState(session.getChatId(), State.MENU);
+            messageService.sendMenuMessage(session.getChatId(), session.getUrl());
+            return;
+
+        } else {
+            log.info("Unknown callback query data={}", data);
+            return;
+        }
     }
 }
