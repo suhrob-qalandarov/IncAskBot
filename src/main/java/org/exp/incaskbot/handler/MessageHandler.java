ec451246dd348d2c8268cd1c0321d7c768ced475
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-26 18:25:59 +0500

    Refactor `MessageHandler` to use `SessionService` instead of `UserService`; enhance incognito message processing with support for various media types.

--- src/main/java/org/exp/incaskbot/handler/MessageHandler.java
+++ src/main/java/org/exp/incaskbot/handler/MessageHandler.java
@@ -1,12 +1,12 @@
 package org.exp.incaskbot.handler;
 
 import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.passport.PassportData;
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
-import org.exp.incaskbot.model.User;
+import org.exp.incaskbot.model.Session;
+import org.exp.incaskbot.model.enums.State;
 import org.exp.incaskbot.service.face.MessageService;
-import org.exp.incaskbot.service.face.UserService;
+import org.exp.incaskbot.service.face.SessionService;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
@@ -17,28 +17,59 @@ import java.util.function.Consumer;
 public class MessageHandler implements Consumer<Message> {
 
     private final MessageService messageService;
-    private final UserService userService;
+    private final SessionService sessionService;
 
     @Override
     public void accept(Message message) {
         String text = message.text();
-        User dbUser = userService.getOrCreateUser(message.from());
+        Session session = sessionService.getOrCreateSession(message.from());
 
-        System.out.println(message);
+        if (session.getState().equals(State.MESSAGE)) {
+            if (text != null && !text.isBlank()) {
+                messageService.sendIncognitoTextMessage(session, text);
+
+            } else if (message.audio() != null) {
+                messageService.sendIncognitoAudioMessage(session, message.audio());
+
+            } else if (message.video() != null) {
+                messageService.sendIncognitoVideoMessage(session, message.video());
+
+            } else if (message.animation() != null) {
+                messageService.sendIncognitoAnimationMessage(session, message.animation());
+
+            } else if (message.voice() != null) {
+                messageService.sendIncognitoVoiceMessage(session, message.voice());
+
+            }  else if (message.photo() != null) {
+                messageService.sendIncognitoPhotoMessage(session, message.photo());
+
+            } else if (message.videoNote() != null) {
+                messageService.sendIncognitoVideoNoteMessage(session, message.videoNote());
+
+            } else if (message.sticker() != null) {
+                messageService.sendIncognitoStickerMessage(session, message.sticker());
+            } else {
+                log.info("Unknown message={}", message);
+            }
+        }
 
         if (text!=null) {
             if (text.startsWith("/start")) {
                 String[] parts = message.text().split(" ", 2);
-                String payload = parts.length > 1 ? parts[1] : null;
+                String startParam = parts.length > 1 ? parts[1] : null;
 
-                if (payload != null) {
-                    System.out.println("Start param: " + payload);
+                if (startParam != null) {
+                    messageService.sendParamMenuMessage(session.getChatId());
+                    sessionService.updateUserSessionParam(session.getChatId(), startParam);
+                    return;
                 } else {
-                    messageService.sendMenuMessage(dbUser);
+                    messageService.sendMenuMessage(session.getChatId(), session.getUrl());
+                    return;
                 }
 
             } else if (text.equals("/info")) {
                 System.out.println("info");
+                return;
             }
         }
     }
