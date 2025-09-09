ec1d302dca19ad7a1dfff35349d8244301063af9
Suhrob <suhrob_kalandarov@outlook.com>
2025-09-10 00:01:51 +0500

    Refactor `IncChatRepository` to use `IncognitoChat` entity.

--- src/main/java/org/exp/incaskbot/repository/IncChatRepository.java
+++ src/main/java/org/exp/incaskbot/repository/IncChatRepository.java
@@ -1,9 +1,9 @@
 package org.exp.incaskbot.repository;
 
-import org.exp.incaskbot.model.entity.IncChat;
+import org.exp.incaskbot.model.entity.IncognitoChat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IncChatRepository extends JpaRepository<IncChat, Long> {
+public interface IncChatRepository extends JpaRepository<IncognitoChat, Long> {
 }
