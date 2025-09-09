6e937b8ab813ff47cd6afbc3e29efe44aac9c90a
Suhrob <suhrob_kalandarov@outlook.com>
2025-09-10 00:02:09 +0500

    Implement `IncognitoUriServiceImpl` with placeholder methods `setTemporaryUriToUser` and `checkIsEmptyUri`.

---
+++ src/main/java/org/exp/incaskbot/service/impl/IncognitoUriServiceImpl.java
@@ -0,0 +1,19 @@
+package org.exp.incaskbot.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.exp.incaskbot.model.entity.User;
+import org.exp.incaskbot.service.face.IncognitoUriService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class IncognitoUriServiceImpl implements IncognitoUriService {
+
+    public void setTemporaryUriToUser(User user){
+
+    }
+
+    public void checkIsEmptyUri(){
+
+    }
+}
